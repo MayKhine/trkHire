20046bf05da32de147bbf54e4c4a06d09c851d1a
May <maykhine.ada@gmail.com>
2025-02-24 12:55:47 -0500

    wroking on legend colors

--- src/components/charts/ColorPicker.tsx
+++ src/components/charts/ColorPicker.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react"
 import { HexColorPicker } from "react-colorful"
 
-type LegendProps = {
+type ColorPickerProps = {
   // rejected: string
   // offer: string
   // saved: string
   // applied: string
   // interview: string
   text: string
-  initalColor: string
+  color: string
+  onColorChangeHandler: (val: string) => void
 }
-export const Legend = ({ text, initalColor }: LegendProps) =>
+export const ColorPicker = ({
+  text,
+  color,
+  onColorChangeHandler,
+}: ColorPickerProps) =>
   //   {
   //   rejected,
   //   offer,
@@ -20,14 +25,14 @@ export const Legend = ({ text, initalColor }: LegendProps) =>
   // }: LegendProps
 
   {
-    const [color, setColor] = useState(initalColor)
+    // const [color, setColor] = useState(initalColor)
     const [colorPickerOpen, setColorPickerOpen] = useState(false)
 
     return (
-      <div className="bg-green-300 m-4">
-        <div>
+      <div className="bg-green-300 m-4 p-4">
+        <div className="flex flex-col justify-center items-center">
           <div
-            className="w-10 h-10 border-2 m-6"
+            className="w-10 h-10 border-2 cursor-pointer"
             style={{ backgroundColor: color }}
             onClick={() => {
               setColorPickerOpen(!colorPickerOpen)
@@ -37,20 +42,20 @@ export const Legend = ({ text, initalColor }: LegendProps) =>
         </div>
         {colorPickerOpen && (
           <div
-            // className="z-15 w-full h-full fixed left-0 top-0 bg-red-300/20"
             onClick={() => {
               setColorPickerOpen(!colorPickerOpen)
             }}
           >
-            <div className="z-15 w-full h-full fixed left-0 top-0 bg-red-300/20"></div>
+            <div className="z-15 w-full h-full fixed left-0 top-0 "></div>
             <div
               className="z-20 absolute"
               onClick={(event) => event.stopPropagation()}
             >
               <HexColorPicker
+                style={{ cursor: "pointer" }}
                 color={color}
                 onChange={(val) => {
-                  setColor(val)
+                  onColorChangeHandler(val)
                 }}
               />
             </div>
--- src/components/pages/HomePg.tsx
+++ src/components/pages/HomePg.tsx
@@ -2,7 +2,8 @@ import { MenuBar } from "../menu/MenuBar"
 import { jobByStatusColumns } from "../../utils/data"
 import { jobByStatusColumnsType, reChartPieDataType } from "../../utils/types"
 import { PieChartGraph } from "../charts/PieChartGraph"
-import { Legend } from "../charts/Legend"
+import { ColorPicker } from "../charts/ColorPicker"
+import { useState } from "react"
 export const HomePg = () => {
   const getTheJobsByStatus = (
     jobByStatusColumns: Array<jobByStatusColumnsType>
@@ -14,18 +15,42 @@ export const HomePg = () => {
   }
 
   const jobData = getTheJobsByStatus(jobByStatusColumns)
+  const [legendColorArr, setLegendColorArr] = useState([
+    { id: "saved", color: "green" },
+    { id: "applied", color: "blue" },
+    { id: "interview", color: "yellow" },
+    { id: "offer", color: "pink" },
+    { id: "rejected", color: "red" },
+  ])
+
+  console.log("legend Color arr: ", legendColorArr[0])
   return (
     <div className="flex w-full h-full bg-amber-300">
       <MenuBar />
-      <div className="bg-pink-600 w-full h-full overflow-hidden flex flex-col">
+      <div className="bg-pink-200 w-full h-full overflow-hidden flex flex-col relative">
         <div className="text-2xl font-bold p-6 border-2 border-l-0">
           Welcome
         </div>
-        <div className="bg-pink-200 gap-6 flex flex-col h-full border-b-2 border-r-2">
-          <div className="flex bg-amber-200">
+        <div className="bg-amber-200 gap-6 flex flex-col h-full border-b-2 border-r-2 overflow-x-auto max-w-[100%] ">
+          <div className="flex bg-amber-600 relative">
             Legend
-            <Legend initalColor="#388743" text="saved" />
-            <Legend initalColor="#388743" text="applied" />
+            {legendColorArr.map((e, index) => {
+              return (
+                <ColorPicker
+                  key={index}
+                  color={e.color}
+                  text={e.id}
+                  onColorChangeHandler={(val: string) => {
+                    console.log("color change called", e.id, val)
+                    setLegendColorArr((prev) => {
+                      return prev.map((item) => {
+                        return item.id === e.id ? { ...item, color: val } : item
+                      })
+                    })
+                  }}
+                />
+              )
+            })}
           </div>
           <div className="overflow-x-auto max-w-[100%] h-full">
             <div> Colors </div>
